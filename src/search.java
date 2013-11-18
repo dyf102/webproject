94bc72bae031a759cef8a4b58c92647e7af728c6
Yu Wei Duan <yuwei@cs.ualberta.ca>
2013-11-17 18:44:08 -0700

    hi

---
+++ src/search.java
@@ -0,0 +1,9 @@
+package com.cmp391.servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+public class HttpSearch extends HttpServlet{
+	public void doGet(HttpServletRequest req,HttpServletResponse resp)
+		throws ServletException,java.io.IOException{
+			HttpSession ses = req.getSession();
+		}
+}
